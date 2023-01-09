a2eb7759cba360f21941525c9573af1baa44ffef
emirhanpaksoy <77emirbatu77@gmail.com>
2023-01-09 16:23:33 +0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -3,7 +3,7 @@ CREATE SEQUENCE property_id_seq INCREMENT 1 START 1;
 CREATE SEQUENCE property_feature_id_seq INCREMENT 1 START 1;
 CREATE SEQUENCE land_registry_id_seq INCREMENT 1 START 1;
 
-CREATE TABLE user (
+CREATE TABLE users (
 	ssn NUMERIC PRIMARY KEY NOT NULL DEFAULT nextval('user_id_seq'),
 	fname VARCHAR(64) NOT NULL,
 	lname VARCHAR(32) NOT NULL,
@@ -12,15 +12,15 @@ CREATE TABLE user (
 	phone_number VARCHAR(16),
 	email VARCHAR(64),
 	address VARCHAR(256),
-	wallet DOUBLE NOT NULL,
-	role VARCHAR(16) NOT NULL
+	wallet NUMERIC(15,2) NOT NULL,
+	user_role VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE property (
 	property_id NUMERIC PRIMARY KEY NOT NULL DEFAULT nextval('property_id_seq'),
 	address VARCHAR(256) NOT NULL,
 	property_type VARCHAR(64) NOT NULL,
-	property_value DOUBLE,
+	property_value NUMERIC(15,2),
 	area NUMERIC(6,1)
 );
 
@@ -38,42 +38,39 @@ CREATE TABLE land_registry (
 	property_id NUMERIC NOT NULL,
 	buyer_id NUMERIC NOT NULL,
 	seller_id NUMERIC NOT NULL,
-	price DOUBLE NOT NULL,
+	price NUMERIC(15,2) NOT NULL,
 	issued_at DATE DEFAULT now(),
 	isActive BOOLEAN NOT NULL DEFAULT true,
 	
 	FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE CASCADE,
-	FOREIGN KEY (buyer_id) REFERENCES user(user_id) ON DELETE CASCADE,
-	FOREIGN KEY (seller_id) REFERENCES user(user_id) ON DELETE CASCADE
+	FOREIGN KEY (buyer_id) REFERENCES users(ssn) ON DELETE CASCADE,
+	FOREIGN KEY (seller_id) REFERENCES users(ssn) ON DELETE CASCADE
 );
 
-ALTER TABLE user ADD CONSTRAINT check_role CHECK (role IN ('Customer', 'Employee'));
+ALTER TABLE users ADD CONSTRAINT check_role CHECK (user_role IN ('Customer', 'Employee'));
 
 CREATE VIEW user_land_registry AS
-	SELECT ssn, fname, lname, birth_date, gender, phone_number, email, address, wallet, role, land_registry_id, property_id, price, date, 
-	FROM user INNER JOIN land_registry ON user.user_id = land_registry.buyer_id;
+	SELECT ssn, fname, lname, birth_date, gender, phone_number, email, address, wallet, user_role, land_registry_id, property_id, price, issued_at 
+	FROM users INNER JOIN land_registry ON users.ssn = land_registry.buyer_id;
 
 CREATE VIEW land_registry_property AS
-	SELECT land_registry_id, buyer_id, price, date, isActive, property_id, address, property_type, property_value, area
+	SELECT land_registry_id, buyer_id, price, issued_at, isActive, property.property_id, address, property_type, property_value, area
 	FROM land_registry INNER JOIN property ON land_registry.property_id = property.property_id;
 
-CREATE TRIGGER circulate_capital
-BEFORE INSERT
-ON land_registry
-FOR EACH ROW EXECUTE PROCEDURE sell_property();
+
 
 CREATE OR REPLACE FUNCTION sell_property()
 RETURNS TRIGGER AS $$
 DECLARE
-	amount_to_share DOUBLE;
+	amount_to_share NUMERIC(15,2);
 	number_of_employees NUMERIC;
-	price DOUBLE;
-	buyer_wallet DOUBLE;
-	seller_wallet DOUBLE;
+	price NUMERIC(15,2);
+	buyer_wallet NUMERIC(15,2);
+	seller_wallet NUMERIC(15,2);
 BEGIN
 	-- Get wallets of buyer and seller into variables
-	SELECT wallet INTO buyer_wallet FROM user WHERE user_id = new.buyer_id;
-	SELECT wallet INTO seller_wallet FROM user WHERE user_id = new.seller_id;
+	SELECT wallet INTO buyer_wallet FROM users WHERE user_id = new.buyer_id;
+	SELECT wallet INTO seller_wallet FROM users WHERE user_id = new.seller_id;
 	
 	-- Check if both buyer and seller have enough money for 2.5% cut
 	IF buyer_wallet < new.price * 1.025 THEN
@@ -81,18 +78,24 @@ BEGIN
 	END IF;
 	
 	-- Update wallets of buyer and seller
-	UPDATE user SET wallet = wallet - new.price * 1.025 WHERE user_id = buyer_id;
-	UPDATE user SET wallet = wallet + new.price * 0.975 WHERE user_id = seller_id;
+	UPDATE users SET wallet = wallet - new.price * 1.025 WHERE user_id = buyer_id;
+	UPDATE users SET wallet = wallet + new.price * 0.975 WHERE user_id = seller_id;
 	
 	-- How many employees do we have?
-	SELECT count(*) INTO number_of_employees FROM user WHERE role = 'Employee';
+	SELECT count(*) INTO number_of_employees FROM users WHERE user_role = 'Employee';
 	
 	-- Calculate how much money we will send to wallets of each employee
 	amount_to_share := new.price * 0.05 / number_of_employees;
 	
 	-- Send money to wallets
-	UPDATE user SET wallet = wallet + amount_to_share WHERE role = 'Employee';
+	UPDATE users SET wallet = wallet + amount_to_share WHERE user_role = 'Employee';
 	
 	RETURN new;
 END;
 $$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER circulate_capital
+BEFORE INSERT
+ON land_registry
+FOR EACH ROW EXECUTE PROCEDURE sell_property();
